3e0713cf2898017daf2620a4b6661023c4b35ef3
Robinson Opondo Wesonga <wesongarobinson2007@gmail.com>
2026-03-26 20:01:13 +0300

    formatted the search button of the website. corrected all the spelling errors in the project. inserted the javascript file to the project and made some changes to the html and css files to link the script and add some functionality. modified the blog, index, contact, about and services pages to add a button that will trigger a function in the script file to display an alert message when clicked. also added some styling to the button in the css file. inserted a readme file to the project and added some information about the project and how to run it.

--- Script.js
+++ Script.js
@@ -1,20 +1,29 @@
-const text = " Robinson wesonga";
+let text = " Robinson wesonga";
 ;
 
-let i=0;
+let i = 0;
 
-function typing(){
+function typing() {
 
-if(i<text.length){
+    if (i < text.length) {
 
-document.querySelector(".typing").innerHTML += text.charAt(i);
+        document.querySelector(".typing").innerHTML += text.charAt(i);
 
-i++;
+        i++;
 
-setTimeout(typing,100);
+        setTimeout(typing, 100);
 
-}
+    }
 
 }
 
 typing();
+
+function showtime() {
+    const now = new Date();
+    const time = now.toLocaleTimeString();
+    document.getElementById("timebox").
+        textContent = time;
+}
+setInterval(showtime, 1000);
+showtime();
